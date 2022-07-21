bfc259be70018b0a0148e7898bace0e3908829b5
Szymon <sjedliczka@gmail.com>
2022-07-21 23:36:13 +0200

    .

--- server/config/default.ts
+++ server/config/default.ts
@@ -4,7 +4,7 @@ export default {
     private_key: "test",
     private_rfsh_key: "tefdsfdsfdsfdsfst2",
     expiresIn: "30m",
-    GEO_API_KEY: '2aabbec3587c02ac7473b524db7fa71c',
+    GEO_API_KEY: '',
     GEO_API_URL: 'http://api.ipstack.com/',
     frontend_url: 'http://localhost:5001'
 }
